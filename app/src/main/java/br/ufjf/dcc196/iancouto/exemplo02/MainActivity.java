9e61f43f6e4152b4ddede5c742873278f8c07ab4
IanCouto <iancouto@ice.ufjf.br>
2022-05-18 22:44:58 -0300

    adiciona elementos

--- app/src/main/java/br/ufjf/dcc196/iancouto/exemplo02/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/iancouto/exemplo02/MainActivity.java
@@ -3,6 +3,8 @@ package br.ufjf.dcc196.iancouto.exemplo02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clicaBotaoMensagem(View orige){
+        TextView textViewMessage = findViewById(R.id.textViewMessage);
+        String text = textViewMessage.getText().toString();
+
+        TextView editTextNome = findViewById(R.id.editTextNome);
+        TextView editTextStarter = findViewById(R.id.editTextStarterName);
+
+        String nome = editTextNome.getText().toString();
+        String starter = editTextStarter.getText().toString();
+        String mensagem = String.format("Olá treinador %s! Seu starter é %s!",
+                nome,
+                starter);
+        textViewMessage.setText(mensagem);
     }
 }
